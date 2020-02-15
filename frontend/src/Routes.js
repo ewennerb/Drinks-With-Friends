0f690f914b6a0222f92e10592b7e1b1290fad808
rod <rgerbasi98@gmail.com>
2020-02-14 19:32:21 -0500

    added semantic ui library and stated working on profile

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter,Route, Switch } from "react-router-dom";
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Search from "./pages/Search"
@@ -9,12 +9,14 @@ export default class Routes extends React.Component {
 
     render(){
         return (
+            <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={Search} />
                 <Route exact path="/login" component={Login} />
-                <Route exact path="/register" compnent={Register}/>
+                <Route exact path="/register" component={Register}/>
                 <Route exact path="/profile" component={Profile}/>
             </Switch>
+            </BrowserRouter>
         );
     }
 }
