993a4ae6ea76fc2882fc8026246bd67ec16b4531
Rodrigo Gerbasi <rgerbasi98@gmail.com>
2020-02-16 18:52:29 -0500

    changed the date option

--- getcommits.sh
+++ getcommits.sh
@@ -2,4 +2,4 @@
 
 name="$1 $2"
 echo "commits for " $name
-git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --author="$name"
+git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset' --abbrev-commit --author="$name"
