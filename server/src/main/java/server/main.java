8723c5eca9d4854c53da6e87eab81d3fe346e54a
n1ck-kk <nick@leuer.com>
2020-02-17 19:53:38 -0500

    Adding mysql jar and test query in main

--- server/src/main/java/server/main.java
+++ server/src/main/java/server/main.java
@@ -2,6 +2,7 @@ package server;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
+import java.sql.*;
 
 @RestController
 @SpringBootApplication
@@ -9,7 +10,27 @@ public class main {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+
+	try{
+		String url = "jdbc:mysql://localhost:3306/";
+		Connection conn = DriverManager.getConnection(url, "root", "1234DrinksWithFriends");
+		Statement smt = conn.createStatement();
+		ResultSet rs = smt.executeQuery("select userName from test_schema.user where userId=1");
+		String all="";
+		while (rs.next())
+		{
+			all+=rs.getString("userName");
+			System.out.println(all);
+		}
+		conn.close();
+		return all;
+	}catch(Exception e){
+		e.printStackTrace();
+		return "FAIL";
+	}
+
+
+        //return "Hello World!";
     }
 
     public static void main(String[] args) {
