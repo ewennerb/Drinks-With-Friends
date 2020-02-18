4e7dd6f551f757a41f740930b3872217714426cb
edward <edward@genomoncology.com>
2020-02-17 19:36:18 -0500

    Added Dropdown menu to the navigation bar

--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -1,10 +1,14 @@
 import React from "react";
 import 'semantic-ui-css/semantic.min.css';
 
-export default class Search{
+
+export default class Search extends React.Component{
+
     render(){
+
         return(
-            <div/>
+            <div>
+            </div>
         )
     }
 }
