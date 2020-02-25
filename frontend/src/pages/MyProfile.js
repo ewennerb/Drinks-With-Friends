4ae2fe1a2621d6a318ba402b543b32cb99a7850b
rod <rgerbasi98@gmail.com>
2020-02-24 20:01:15 -0500

    trying to figure out a way to render differnet user pages created a new component as a test for the router need to figure out how to mess with the paths

--- frontend/src/pages/MyProfile.js
+++ frontend/src/pages/MyProfile.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import 'semantic-ui-css/semantic.min.css';
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import {
   Menu,
   Grid,
@@ -8,9 +9,10 @@ import {
   Container,
   Modal,
   Header,
-  Feed,
-
+  Segment,
+  
 } from "semantic-ui-react";
+import LikedDrinks from "./LikedDrinks.js"
 
 
 //import "../css/Profile.css"
@@ -21,7 +23,10 @@ class Profile extends Component{
     
     this.handleClose = this.handleClose.bind(this);
     this.handleOpen = this.handleOpen.bind(this);
-    this.state = {modalOpen: false, activeItem: "posts"};
+    this.state = {
+      modalOpen: false, 
+      activeItem: "posts"
+    };
 }
 
 handleClose() {
@@ -39,7 +44,6 @@ handleOpen() {
         }
     )
 }
-
   
 
   handleItemClick = (e, {name}) => {
@@ -52,94 +56,105 @@ handleOpen() {
 
     return(
       <Container>
-        
-        <Grid className="grid" columns={3} container padded relaxed textAlign="center">
+        <BrowserRouter>
+          <Grid className="grid" columns={3} container padded relaxed textAlign="center">
           <Grid.Row container>
-            <Grid.Column color="blue">
-              <Icon name="user circle outline" size="massive"/>
-            </Grid.Column>
+          <Grid.Column color="blue">
+          <Icon name="user circle outline" size="massive"/>
+          </Grid.Column>
           
-            <Grid.Column >
-           
-            </Grid.Column>
-
-            <Grid.Column color="red">
-              <Button animated="fade" onClick={this.handleOpen}>
-                <Button.Content visible>Edit Profile</Button.Content>
-                <Button.Content hidden>
-                  <Icon name="edit"/>
-                </Button.Content>
-              </Button>
-              
-            </Grid.Column>/>
+          <Grid.Column >
+          
+          </Grid.Column>
+          
+          <Grid.Column color="red">
+          <Button animated="fade" onClick={this.handleOpen}>
+          <Button.Content visible>Edit Profile</Button.Content>
+          <Button.Content hidden>
+          <Icon name="edit"/>
+          </Button.Content>
+          </Button>
+          
+          </Grid.Column>/>
           </Grid.Row>
-
+          
           <Grid.Row >
           
-            <Grid.Column width={10} color="green" >
-            <Menu  pointing secondary stackable >
-              <Menu.Item
-                name="posts"
-                active={activeItem === "posts"}
-                onClick={this.handleItemClick}
-              />
-              <Menu.Item
-                name="likedDrinks"
-                active={activeItem === "likedDrinks"}
-                onClick={this.handleItemClick}
-              />
-              <Menu.Item
-                name="dislikedDrinks"
-                active={activeItem === "dislikedDrinks"}
-                onClick={this.handleItemClick}
-              />
-              <Menu.Item
-                name="map"
-                active={activeItem === "map"}
-                onClick={this.handleItemClick}              
-
-              />
-              <Menu.Item
-                name="friends"
-                active={activeItem === "friends"}
-                onClick={this.handleItemClick}              
-              />
-
-
-            </Menu> 
-
-            </Grid.Column>
-
-            
-            </Grid.Row>
-
-            
-            <Grid.Column>
-            lists of shit
-
-            </Grid.Column>
-          <Grid.Row>
-          <Feed>
-
-
-          </Feed>
-
+          <Grid.Column width={10} color="green" >
+          <Menu  pointing secondary stackable >
+          <Menu.Item
+          //  this one will be hard to decide how to do 
+           name="posts"
+           
+            active={activeItem === "posts"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name="likedDrinks"
+            as={Link}
+            to={{pathname: './likedDrinks'}}
+            active={activeItem === "likedDrinks"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name="dislikedDrinks"
+            as={Link}
+            to={{pathname: './dislikedDrings'}}
+            active={activeItem === "dislikedDrinks"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name="map"
+            active={activeItem === "map"}
+            onClick={this.handleItemClick}
+          
+          />
+          <Menu.Item
+            name="friends"
+            active={activeItem === "friends"}
+            onClick={this.handleItemClick}
+          />
+          
+          </Menu>
+          </Grid.Column>
           </Grid.Row>
-        </Grid>
+          
+          <Grid.Column>
+          lists of shit
+          
+          </Grid.Column>
+          <Grid.Row>
+          <Segment basic placeholder>
+            <Switch>
+              <Route exact path="./likedDrinks" component={LikedDrinks}/>
 
-        <Modal
+            </Switch>
+          </Segment>
+          </Grid.Row>
+          </Grid>
+          
+
+
+
+
+
+
+
+          {/* profile edit modal */}
+          <Modal
           open={this.state.modalOpen}
           onClose={this.handleClose}
           size="large">
           <Modal.Content image >
-              {/*display current profile info with the option to change it */}
-              
-              <Header as='h2' color='grey' textAlign='center'>Edit Profile</Header>            
-              {/* need to add profile picture and how to store kind of confused */}
-            
+          {/*display current profile info with the option to change it */}
+          
+          <Header as='h2' color='grey' textAlign='center'>Edit Profile</Header>
+          {/* need to add profile picture and how to store kind of confused */}
+          
           </Modal.Content>
-        </Modal>
-
+          </Modal>
+          
+        </BrowserRouter>
       </Container>  
     )
   }
