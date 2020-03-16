353f8faab2589d3e8a3596b43ebb922f112093c2
edward <edward@genomoncology.com>
2020-03-15 20:47:13 -0500

    Ingredients Search Work

--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -32,7 +32,6 @@ export default class Search extends React.Component{
             loggedIn: false,
             done: false,
             results: [],
-            // is21: this.props.location.state.is21,
             searchable: false,
         }
     }
@@ -46,10 +45,16 @@ export default class Search extends React.Component{
             user: this.props.user,
             done: true,
             results: [],
-            searchable: false
+            searchable: false,
+            searchVal: "d",
         })
     }
 
+    handleSettingsChange = (e, { value }) => {
+        console.log(value);
+        this.setState({ searchVal: value });
+        console.log(this.state.value);
+    };
 
     //Fetches the Drink of the Day
     async getDOTD(){
@@ -74,7 +79,17 @@ export default class Search extends React.Component{
 
     //Todo: Send the query parameters to the server and fuck shit up
     async getSearchResults(){
-        await fetch('http://localhost:8080/drink/search?s=' + this.state.searchText, {
+
+        let url;
+        if (this.state.searchVal === 'u'){
+            url = "http://localhost:8080/user/" + this.state.searchText;
+        }else if(this.state.searchVal === "d"){
+            url = "http://localhost:8080/drink/search?s=" + this.state.searchText;
+        }else if(this.state.searcVal === "i"){
+            url = "http://localhost:8080/ingredients/search?s=" + this.state.searchText;
+        }
+        console.log(url);
+        await fetch(url, {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
@@ -83,7 +98,7 @@ export default class Search extends React.Component{
         }).then(res => res.json()).then(async (data) => {
             console.log(data);
             this.setState({results: data.results})
-        }).catch(console.log);
+        }).catch(this.setState({results: []}));
         this.setState({loaded: true})
     }
 
@@ -101,25 +116,25 @@ export default class Search extends React.Component{
     };
 
 
-    render(){
+    render() {
 
-        if (!this.state.done){
-            return(
+        const value = this.state.searchVal;
+
+        if (!this.state.done) {
+            return (
                 <div>
-                    <Segment style={{ height: '40vh' }} textAlign={"center"}>
+                    <Segment style={{height: '40vh'}} textAlign={"center"}>
                         <Dimmer active>
-                            <Loader content='Loading' />
+                            <Loader content='Loading'/>
                         </Dimmer>
                     </Segment>
                 </div>
             )
-        }else{
-
-        }
+        } else {
             //IF dotd not ready return loader
-            return(
+            return (
                 <div>
-                    <Grid style={{ height: '100vh', overflow:"scroll" }} columns={16} centered>
+                    <Grid style={{height: '100vh', overflow: "scroll"}} columns={16} centered>
                         <Grid.Column width={4}/>
                         <Grid.Column width={8} textAlign="center">
                             <br/>
@@ -127,21 +142,15 @@ export default class Search extends React.Component{
                             <Card style={{width: "500px"}} centered>
                                 <Card.Header>Today's Drink of the Day</Card.Header>
                                 <Segment basic textAlign="left" attached="bottom" style={{width: "500px"}}>
-                                    {/*<Image*/}
-                                    {/*    floated='left'*/}
-                                    {/*    size='small'*/}
-                                    {/*    src='https://react.semantic-ui.com/images/avatar/large/molly.png'*/}
-                                    {/*/>*/}
                                     <Header textAlign="center" style={{marginTop: "0px"}}>
                                         {this.state.dotd.name}
                                     </Header>
-                                    {/*<Card.Content header={this.state.dotd.description}/>*/}
                                     <Card.Description content={this.state.dotd.description}/>
                                     <br/>
                                     <Card.Content>
                                         <List bulleted>
                                             {this.state.dotd.ingredients.map(ingr => {
-                                                return(
+                                                return (
                                                     <List.Item>
                                                         {ingr.quantity} {ingr.measurement} {ingr.ingredient}
                                                     </List.Item>
@@ -149,10 +158,7 @@ export default class Search extends React.Component{
                                             })}
                                         </List>
                                     </Card.Content>
-
-                                    {/*<Card.Meta>{this.state.dotd.publisher}</Card.Meta>*/}
                                     <Card.Meta>{this.state.dotd.publisher}</Card.Meta>
-
                                     <Card.Content extra>
                                         <Rating icon='star' defaultRating={5} maxRating={5}/>
                                     </Card.Content>
@@ -162,37 +168,31 @@ export default class Search extends React.Component{
                             <br/>
                             <Grid.Row centered>
                                 {/*Todo: Put a button and maybe some options here*/}
-                                <Form.Group inline>
-                                    <label>Size</label>
-                                    <Form.Radio
-                                        label='Drinks'
-                                        value='d'
-                                        checked={value === 'd'}
-                                        // onChange={this.handleChange}
-                                    />
-                                    <Form.Radio
-                                        label='Ingredients'
-                                        value='i'
-                                        checked={value === 'i'}
-                                        // onChange={this.handleChange}
-                                    />
-                                    <Form.Radio
-                                        label='Users'
-                                        value='u'
-                                        checked={value === 'd'}
-                                        // onChange={this.handleChange}
-                                    />
-                                </Form.Group>
+                                <Form>
+                                    <Form.Group inline>
+                                        <label>Search Options</label>
+                                        <Form.Radio
+                                            label='Drinks'
+                                            value='d'
+                                            checked={value === 'd'}
+                                            onClick={this.handleSettingsChange}
+                                        />
+                                        <Form.Radio
+                                            label='Ingredients'
+                                            value='i'
+                                            checked={value === 'i'}
+                                            onClick={this.handleSettingsChange}
+                                        />
+                                        <Form.Radio
+                                            label='Users'
+                                            value='u'
+                                            checked={value === 'u'}
+                                            onClick={this.handleSettingsChange}
+                                        />
+                                    </Form.Group>
+                                </Form>
 
                                 <Input
-                                    // action={{
-                                    //     color: 'yellow',
-                                    //     labelPosition: 'left',
-                                    //     icon: 'search',
-                                    //     content: 'Search',
-                                    //     onClick: this.getSearchResults()
-                                    // }}
-                                    // actionPosition='right'
                                     size="huge"
                                     fluid
                                     placeholder='Search...'
@@ -210,6 +210,7 @@ export default class Search extends React.Component{
                                 <br/>
                                 <br/>
                                 {this.state.results.map(result => {
+                                    //Todo: Add logic here to figure out what type of results to render
                                     return (
                                         <Card style={{width: "500px"}} centered>
                                             <Segment basic textAlign="left" attached="bottom" style={{width: "500px"}}>
@@ -221,7 +222,7 @@ export default class Search extends React.Component{
                                                 <Card.Content>
                                                     <List bulleted>
                                                         {result.ingredients.map(ingr => {
-                                                            return(
+                                                            return (
                                                                 <List.Item>
                                                                     {ingr.quantity} {ingr.measurement} {ingr.ingredient}
                                                                 </List.Item>
@@ -250,15 +251,5 @@ export default class Search extends React.Component{
                 </div>
             )
         }
-
-
-
-
-        // if (this.state.loaded){
-        //
-        // }else{
-        //     return(
-        //         <div/>
-        //     )
-        // }
+    }
 }
